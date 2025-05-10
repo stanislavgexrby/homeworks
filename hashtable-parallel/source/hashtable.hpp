9cf2abf664689888604cf30bbdb91fc69b65f2b8
stanislav <stasereser@gmail.com>
2025-05-10 23:43:18 +0300

    refreshed logic

--- hashtable-parallel/source/hashtable.hpp
+++ hashtable-parallel/source/hashtable.hpp
@@ -14,33 +14,169 @@ struct Value {
     }
 };
 
-class HashTable {
+class Chain {
 private:
     struct Node {
         int key;
         Value value;
         Node* next;
-        mutable std::shared_mutex mut;
+        mutable std::mutex node_mtx;
 
         Node(int k, const Value& v) : key(k), value(v), next(nullptr) {}
     };
 
-    std::vector<Node*> chain;
-    size_t _size;
-
-    std::hash<int> hash_fn;
-
-    Node* get_chain(int key) const {
-        return chain[hash_fn(key) % _size];
+    Node* head = nullptr;
+    mutable std::shared_mutex chain_mtx;
+public:
+    ~Chain() {
+        clear();
     }
 
+    void Put(int key, const Value& value) {
+        Node* new_node = new Node(key, value);
+        std::unique_lock<std::shared_mutex> chain_lock(chain_mtx);
+
+        if (!head) {
+            head = new_node;
+            return;
+        }
+
+        Node* prev = nullptr;
+        Node* curr = head;
+        std::unique_lock<std::mutex> curr_lock(curr->node_mtx);
+        chain_lock.unlock();
+
+        while (true) {
+            if (curr->key == key) {
+                curr->value = value;
+                delete new_node;
+                return;
+            }
+
+            if (!curr->next) {
+                curr->next = new_node;
+                return;
+            }
+
+            Node* next = curr->next;
+            std::unique_lock<std::mutex> next_lock(next->node_mtx);
+            if (prev) {
+                prev->node_mtx.unlock();
+            }
+
+            prev = curr;
+            curr = next;
+            curr_lock.swap(next_lock);
+        }
+    }
+
+    bool Remove(int key) {
+        std::unique_lock<std::shared_mutex> chain_lock(chain_mtx);
+        if (!head) {
+            return false;
+        }
+
+        Node* prev = nullptr;
+        Node* curr = head;
+        std::unique_lock<std::mutex> curr_lock(curr->node_mtx);
+        if (curr->key == key) {
+            head = curr->next;
+            curr_lock.unlock();
+            delete curr;
+            return true;
+        }
+
+        chain_lock.unlock();
+
+        while (curr->next) {
+            Node* next = curr->next;
+            std::unique_lock<std::mutex> next_lock(next->node_mtx);
+
+            if (next->key == key) {
+                curr->next = next->next;
+                next_lock.unlock();
+                delete next;
+                return true;
+            }
+
+            if (prev) {
+                prev->node_mtx.unlock();
+            }
+
+            prev = curr;
+            curr = next;
+            curr_lock.swap(next_lock);
+        }
+
+        return false;
+    }
+
+    std::optional<Value> Check(int key) const {
+        std::shared_lock<std::shared_mutex> chain_lock(chain_mtx);
+        Node* curr = head;
+
+        if (!curr) {
+            return std::nullopt;
+        }
+
+        std::unique_lock<std::mutex> curr_lock(curr->node_mtx);
+        chain_lock.unlock();
+
+        while (curr) {
+            if (curr->key == key) {
+                return curr->value;
+            }
+
+            Node* next = curr->next;
+            if (!next) {
+                break;
+            }
+
+            std::unique_lock<std::mutex> next_lock(next->node_mtx);
+            curr_lock.unlock();
+            curr = next;
+            curr_lock.swap(next_lock);
+        }
+
+        return std::nullopt;
+    }
+
+    void clear() {
+        std::unique_lock<std::shared_mutex> chain_lock(chain_mtx);
+        Node* curr = head;
+        while(head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+class HashTable {
+private:
+    std::vector<Chain> container;
+    std::hash<int> hash_fn;
+
 public:
-    HashTable(size_t size = 5);
+    HashTable(size_t size = 1000) : container(size) {};
     ~HashTable();
 
-    void Put(int key, const Value& value);
-    bool Remove(int key);
-    std::optional<Value> Check(int key) const;
+    HashTable(HashTable &&other) noexcept = default;
+    HashTable & operator=(HashTable &&other) noexcept = default;
+
+    void Put(int key, const Value& value) {
+        size_t index = hash_fn(key) % container.size();
+        container[index].Put(key, value);
+    }
+    bool Remove(int key) {
+        size_t index = hash_fn(key) % container.size();
+        return container[index].Remove(key);
+    }
+
+    std::optional<Value> Check(int key) const {
+        size_t index = hash_fn(key) % container.size();
+        return container[index].Check(key);
+    }
 };
 
 bool test();
--- hashtable-parallel/test/test.cpp
+++ hashtable-parallel/test/test.cpp
@@ -1,25 +1,91 @@
 #include "../source/hashtable.hpp"
 #include <print>
+#include <thread>
+#include <iostream>
 
-static bool test1() {
-    return 0;
+static bool test_add() {
+    HashTable table(1000);
+
+    uint32_t n = 1000;
+    for (int i = 0; i < n; i++) {
+        table.Put(i, Value {"hi", i});
+    }
+
+    //std::vector<bool> found(n, false);
+
+    for (int j = 0; j < n; j++) {
+        if (table.Check(j) == std::nullopt) {
+            return false;
+            //found[j] = true;
+        }
+    }
+
+    return true;
+    //return std::all_of(found.begin(), found.end(), [](bool a){ return a; });
+}
+
+static bool test_add_parallel() {
+    HashTable table(1000);
+
+    uint32_t n_per_thread = 347;
+    uint32_t threads_num = 5;
+    std::vector<std::jthread> threads;
+    threads.reserve(threads_num);
+    std::atomic<int> count = 0;
+
+    for (int i = 0; i < threads_num; i++) {
+        threads.emplace_back([&table, n_per_thread, i, &count]() {
+            for (int j = n_per_thread * i; j < n_per_thread * (i + 1); j++) {
+                table.Put(count.load(), {"", j});
+                count.fetch_add(1);
+            }
+        });
+    }
+    std::cout << count.load() << std::endl;
+
+    for (auto &th : threads) {
+        th.join();
+    }
+
+    //auto n = n_per_thread * threads_num;
+    //std::vector<bool> found(n, false);
+
+    //auto node = list._first.load()->next.load();
+    /*while (node != nullptr) {
+        found[node->value.d] = true;
+        // std::println("{}", node->value.d);
+        node = node->next.load();
+    }
+
+    if (list.size() != n) {
+        return false;
+    }*/
+    //std::cout << count.load() << std::endl;
+    for (int i = 0; i < count.load(); i++) {
+        if (table.Check(i) == std::nullopt) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool test() {
     using TestCaseT = std::pair<std::function<bool()>, std::string_view>;
 
     std::vector<TestCaseT> cases {
-      {test1, "test1"},
+        {test_add, "test_add"},
+        {test_add_parallel, "test_add_parallel"}
     };
 
     bool test_passed = true;
     for (auto &&[test_case, name] : cases) {
-      auto result = test_case();
-      std::printf("case: {}; result: {}", name, result ? "passed" : "failed");
+        auto result = test_case();
+        std::println("case: {}; result: {}", name, result ? "passed" : "failed");
 
-      if (!result) {
-        test_passed = false;
-      }
+        if (!result) {
+          test_passed = false;
+        }
     }
 
     return test_passed;
