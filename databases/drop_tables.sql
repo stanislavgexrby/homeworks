a0f4bb124506b439a022c2c68a32b7be04dd8efd
stanislav <stasereser@gmail.com>
2025-05-15 22:26:39 +0300

    fixed drops of tables

--- databases/drop_tables.sql
+++ databases/drop_tables.sql
@@ -2,15 +2,31 @@
 -- Удаление
 ---------------------------------------------------------------
 
+-- Удаление представлений
 DROP VIEW IF EXISTS
     vw_screening_details,
     vw_user_orders,
-    vw_available_seats;
+    vw_available_seats CASCADE;
 
-DROP TABLE IF EXISTS Ticket CASCADE;
-DROP TABLE IF EXISTS "Order" CASCADE;
-DROP TABLE IF EXISTS "User" CASCADE;
-DROP TABLE IF EXISTS Screening CASCADE;
-DROP TABLE IF EXISTS Movie CASCADE;
-DROP TABLE IF EXISTS Hall CASCADE;
-DROP TABLE IF EXISTS Cinema CASCADE;
+-- Удаление триггеров (по одному)
+DROP TRIGGER IF EXISTS hall_capacity_check ON Hall CASCADE;
+DROP TRIGGER IF EXISTS screening_availability_check ON Screening CASCADE;
+DROP TRIGGER IF EXISTS order_price_check ON "Order" CASCADE;
+DROP TRIGGER IF EXISTS ticket_data_check ON Ticket CASCADE;
+
+-- Удаление функций
+DROP FUNCTION IF EXISTS
+    validate_hall_capacity(),
+    validate_screening_availability(),
+    validate_order_price(),
+    validate_ticket_data() CASCADE;
+
+-- Удаление таблиц
+DROP TABLE IF EXISTS
+    Ticket,
+    "Order",
+    "User",
+    Screening,
+    Movie,
+    Hall,
+    Cinema CASCADE;
